c256714ed1c7de678f05d5597d0834936ca75195
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-05-03 16:43:29 +0200

    Implement terraforming research

--- SolarisEconSimulation/Program.fs
+++ SolarisEconSimulation/Program.fs
@@ -6,11 +6,14 @@ type Planet = {
     terraform: int
     economy: int
     worldBuilder: bool
+    research: int
 }
 
 type Player = {
     credits: int
     planets: Planet list
+    terraformingLevel: int
+    researchPoints: int
 }
 
 type Entry = {
@@ -21,11 +24,9 @@ type Entry = {
 
 let economyStart = 5
 
+let researchStart = 1
+
 let terraformingStart = 10
-
-let terraformingLevel = 10
-
-let terraformTotal = terraformingStart + (terraformingLevel * 5)
 
 let ticksPerTurn = 24
 
@@ -34,6 +35,8 @@ let turns = 20
 let expenseConfig = 2
 
 let costMultiplier = 2.5
+
+let terraformingBonusPerLevel = 5
 
 type TickInfo =
     | Turn of int * int
@@ -47,28 +50,43 @@ let terraform (player: Player) =
     let planets = List.map tickOnPlanet player.planets
     { player with planets = planets }
 
-let calcUpgradeCosts (planet: Planet) = ((float expenseConfig) * costMultiplier * (planet.economy + 1 |> float)) / ((planet.terraform |> float) * 0.01) |> floor |> int
+let totalTerraforming player planet = planet.terraform + (player.terraformingLevel * terraformingBonusPerLevel)
 
-let rec upgradePlanet (planet: Planet) credits =
-    let upgradeCost = calcUpgradeCosts planet
+let calcUpgradeCosts (player: Player) (planet: Planet) = ((float expenseConfig) * costMultiplier * (planet.economy + 1 |> float)) / ((totalTerraforming player planet |> float) * 0.01) |> floor |> int
+
+let rec upgradePlanet player (planet: Planet) credits =
+    let upgradeCost = calcUpgradeCosts player planet
     if upgradeCost < credits then
-        upgradePlanet { planet with economy = planet.economy + 1 } (credits - upgradeCost)
+        upgradePlanet player { planet with economy = planet.economy + 1 } (credits - upgradeCost)
     else (planet, credits)
 
 let upgrade (player: Player) =
-    let (planets, credits) = Seq.mapFold (fun cr planet -> upgradePlanet planet cr) player.credits player.planets
+    let (planets, credits) = Seq.mapFold (fun cr planet -> upgradePlanet player planet cr) player.credits player.planets
     { player with planets = Seq.toList planets; credits = credits }
 
 let totalEconomy player = Seq.map (fun (p: Planet) -> p.economy) player.planets |> Seq.sum
 
+let totalResearch player = Seq.map (fun (p: Planet) -> p.research) player.planets |> Seq.sum
+
 let produce (player: Player) =
     { player with credits = player.credits + (totalEconomy player * 10) }
+
+let rec researchTerraforming level rp =
+    let requiredForNextLevel = 100 * level
+    if rp > requiredForNextLevel then
+        researchTerraforming (level + 1) (rp - requiredForNextLevel)
+    else (level, rp)
+
+let research (player: Player) =
+    let researchPoints = player.researchPoints + totalResearch player
+    let (rp, level) = researchTerraforming player.terraformingLevel researchPoints
+    { player with researchPoints = rp; terraformingLevel = level }
 
 let snapshot player turn = { turn = turn; economy = totalEconomy player; credits = player.credits }
 
 let performTurn number player = player |> upgrade |> produce
     
-let performTick tickNumber player = player |> terraform
+let performTick tickNumber player = player |> terraform |> research
     
 let tick player tickInfo =
     match tickInfo with
@@ -101,18 +119,23 @@ let toPlot entries name =
     Scatter(x = turns, y = econ, name = name)
 
 let genPlanet wb = {
-    terraform = terraformTotal
+    terraform = terraformingStart
     economy = economyStart
     worldBuilder = wb
+    research = researchStart
 }
 
 [<EntryPoint>]
 let main argv =    
     let player1 = {
+        terraformingLevel = 1
+        researchPoints = 0
         credits = 0
         planets = Seq.init 10 (fun _ -> genPlanet true) |> Seq.toList
     }
     let player2 = {
+        terraformingLevel = 1
+        researchPoints = 0
         credits = 10000
         planets = Seq.init 10 (fun _ -> genPlanet false) |> Seq.toList
     }
